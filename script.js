fcabbc6f1ccb248b881bab857b05e58e12d514f8
ben <lordezral9@gmail.com>
2024-04-03 17:48:40 +0600

    loader vis/hid

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function imageLoaded() {
     console.log(imagesLoaded)
     if (imagesLoaded === totalImages) {
         ready = true;
+        loader.hidden = true;
         console.log('ready =', ready);
     }
 }
