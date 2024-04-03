87e49986b38e167102ba7d4f760e1a80ddaff10c
ben <lordezral9@gmail.com>
2024-04-03 17:16:27 +0600

    loaded img counter

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imageLoaded = 0;
+let totalImages = 0;
 let photosArray = []; //"let" bc array will be changing
 
 //Unsplash API
 const count = 30;
-const apiKey = "n3hXl8XvaH1D-33CNsa0jZMcC5rBWMT4ZlF93B1N7lI";
+const apiKey = "xIf9b836pQXBF1vfALBmvFkICf3-XxkQdvLRWOXkDZA";/*"n3hXl8XvaH1D-33CNsa0jZMcC5rBWMT4ZlF93B1N7lI";*/
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Check if images were loaded
 function imageLoaded() {
     console.log('image loaded');
+    imageLoaded++;
+    if (imageLoaded === totalImages) {
+        ready = true;
+        console.log('ready =', ready);
+    }
 }
 
 
@@ -23,8 +31,10 @@ function setAttributes(element,attributes) {
 
 //Create Elements for Links and Photos, Add to DOM
 function displayPhotos() {
+    totalImages = photosArray.length;
+    console.log('total images: ', totalImages);
   //run function for each object in photosArray
-  photosArray.forEach((photo) => {
+    photosArray.forEach((photo) => {
     //Create <a> to link to UnSplash
     const item = document.createElement("a");
     // item.setAttribute("href", photo.links.html);
