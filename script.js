efcbe8f152d123a9555970d5738180bd215d7339
ben <lordezral9@gmail.com>
2024-04-03 11:50:05 +0600

    v2 update

--- script.js
+++ script.js
@@ -8,19 +8,32 @@ const count = 10;
 const apiKey = "n3hXl8XvaH1D-33CNsa0jZMcC5rBWMT4ZlF93B1N7lI";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper function to set Attribute on DOM Elements
+function setAttributes(element,attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key,attributes[key])
+    }
+}
+
 //Create Elements for Links and Photos, Add to DOM
 function displayPhotos() {
   //run function for each object in photosArray
   photosArray.forEach((photo) => {
     //Create <a> to link to UnSplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+    });
     //create image for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+    })
     //put img inside <a>, put <a> inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
